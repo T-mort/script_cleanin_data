21bec723023c8712f3b285b4e383cfe8099ed28d
T-mort <j.ortizcabezas@gmail.com>
2025-05-04 00:56:29 -0500

    limpieza de datos y verificacion de nulos

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Commit inical")
+from hmac import digest_size
+
+import pandas as pd
+from numpy.ma.extras import column_stack
+
+archive = pd.read_excel(r"assets/data-main.xlsx")
+print(archive.dtypes)
+print(archive.isnull().sum())
+# Filtrar filas con client_email nulo y mostrar los product_id correspondientes
+products_with_null_emails = archive[archive['client_email'].isnull()]['product_id']
+print(products_with_null_emails)
